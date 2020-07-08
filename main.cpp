973bd1683c825f327e130adb49ff6911830bd756
Khanh S Pham <pham.october@gmail.com>
2020-07-08 11:51:21 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+/*
+*   Khanh Pham - lab # 8
+*
+*   Using the String Class, design, develop, and test a password validation program that requests a
+*   password from the user, and validates it based on the following criteria. The password must be at least 8
+*   characters long, contain at least one number, and at least one upper case letter, and at least one lower
+*   case letter. The user is limited to three (3) attempts and is then locked out (the program ends).
+*/
+
+#include <iostream>
+#include <string>
+#include "password.h"
+
+using namespace std;
+
+int main()
+{
+    int num_of_index = 0, attempts = 1;
+    string pwInput, result;
+    bool tof;
+
+    get_User_Input(pwInput);            /* received user input */
+
+    num_of_index = pwInput.length() + 1;
+
+    /* testing or confirming
+
+    //cout << num_of_index << endl;       // check for pwInput index plus 1
+
+    for (int i = 0; i < num_of_index; i++)
+    {
+        cout << pwInput[i] << endl;
+    }
+
+    cout << get_check(pwInput);           // validate what is true or false
+    */
+    get_check(pwInput, result, tof);
+
+    while(!tof == true || attempts < 3)
+    {
+        display_output(pwInput, result, attempts, tof);
+
+        get_User_Input(pwInput);            /* prompt the user to create password again */
+
+        get_check(pwInput, result, tof);
+
+        attempts++;
+    }
+
+    display_output(pwInput, result, attempts, tof);
+
+    return 0;
+}
---
+++ password.h
@@ -0,0 +1,100 @@
+#ifndef PASSWORD_H_INCLUDED
+#define PASSWORD_H_INCLUDED
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void get_User_Input(string& pwInput)
+{
+    cout << "-----------------------------------------------------------" << endl;
+    cout << "Enter a password that contains: \n"
+         << "\t -At least eight (8) characters. \n"
+         << "\t -At least one number. \n"
+         << "\t -At least one upper case letter. \n"
+         << "\t -At least one lower case letter. \n" << endl;
+    cout << "Password: ";
+    cin >> pwInput;
+    cout << "" << endl;
+}
+
+
+bool get_check(string& character, string& result, bool& tof)
+{
+    bool upperCase = false,
+            lowerCase = false,
+            digitNum = false;
+
+    for (int k = 0; k < character[k]; k++)
+    {
+        if (isupper(character[k]))
+        {
+            upperCase = true;
+        }
+        else if (islower(character[k]))
+        {
+            lowerCase = true;
+        }
+        else if (isdigit(character[k]))
+        {
+            digitNum = true;
+        }
+    }
+
+
+    /* combination of scenario of user password is created if its not met
+        the requirements */
+    if (upperCase && lowerCase && digitNum)
+    {
+        tof = true;
+        return tof;
+    }
+    else if (!upperCase && lowerCase && digitNum)
+    {
+        result = "\tIt does not contain an upper case letter.\n";
+        //return false;
+    }
+    else if (upperCase && !lowerCase && digitNum)
+    {
+        result = "\tIt does not contain an lower case letter.\n";
+        //return false;
+    }
+    else if (upperCase && lowerCase && !digitNum)
+    {
+        result = "\tIt does not contain a number.\n";
+        //return false;
+    }
+    else if (!upperCase && lowerCase && !digitNum)
+    {
+        result = "\tIt does not contain an upper case letter\n"
+                "\tor a number.\n";
+        //return false;
+    }
+}
+
+void display_output(string pwInput, string result, int attempts, bool tof)
+{
+
+    if (tof == true)
+    {
+        cout << "\tCongrats! You have created your password. \n" << endl;
+        exit(1);
+    }
+    else if (attempts < 3)
+    {
+        cout << "\t" << pwInput << " is not a valid password. "<< endl;
+        cout << result << endl;
+        cout << "\tThis is your attempt: " << attempts << "\n" << endl;
+    }
+    else
+    {
+        cout << "\t" << pwInput << " is not a valid password. "<< endl;
+        cout << result << "\n" <<endl;
+        cout << "\n>>>>> \t That was the third attempt. This seesion hase been locked. <<<<< \n" << endl;
+        exit(1);
+    }
+
+
+}
+
+#endif // PASSWORD_H_INCLUDED
